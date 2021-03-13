73100ae825bc0eeb923cdd09e5870fd163f8bb41
root <root@vamsikrishnat0.roam.corp.google.com>
2021-03-13 23:17:30 +0530

    First commit to upload files in github

---
+++ FirstLesson/src/AsciiPattern.java
@@ -0,0 +1,20 @@
+
+public class AsciiPattern 
+{
+public static void main(String args[])
+{
+	int n=69;
+	for(int j=65;j<=n;j++)
+		{
+			for(int i=65;i<=j;i++)
+			{
+				char r= (char)i;
+			System.out.print(r+" ");
+			}
+			System.out.println();
+			
+		}
+}
+}
+
+
---
+++ FirstLesson/src/NumberTree.java
@@ -0,0 +1,16 @@
+
+public class NumberTree {
+	public static void main(String[] args)
+	{ int n=5;
+	for(int j=0;j<=n;j++)
+	{
+			for(int i=0;i<=j;i++)
+				{
+					System.out.print(i+" ");
+					
+				}
+	System.out.println();
+	}
+	
+	}
+}
